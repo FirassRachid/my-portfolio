798f4f9aa47964c94ead6a9046c41e6d7f035d30
feadesjof <feadesjof@example.com>
2026-02-25 15:06:24 +0300

    add CV

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,8 @@
 import "./HomePage.css";
 import MyImag from "../../assets/formal-style.png";
 import { FaGithub } from "react-icons/fa6";
-import CV from "../../public/cv.pdf";
+import CvEn from "../../public/cv-en.pdf";
+import CvAr from "../../public/cv-ar.pdf";
 function HomePage() {
   return (
     <section className="home-page">
@@ -10,12 +11,12 @@ function HomePage() {
         <h1>Firass Rachid</h1>
         <h4>Frontend Developer</h4>
         <button>
-          <a href={CV} download>
+          <a href={CvEn} download>
             Download CV-<small>En</small>
           </a>
         </button>
         <button>
-          <a href={CV} download>Download CV-<small>Ar</small></a>
+          <a href={CvAr} download>Download CV-<small>Ar</small></a>
         </button>
       </div>
       <div className="my-photo">
